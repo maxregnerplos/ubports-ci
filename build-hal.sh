181184ce269b306ecfd4cc0cfb55aac4ecb66085
MaxRegner <59399856+MaxRegner@users.noreply.github.com>
2022-12-13 15:50:30 +0100

    Update build-hal.sh

--- build-hal.sh
+++ build-hal.sh
@@ -5,11 +5,8 @@ cd $ANDROID_ROOT
 source build/envsetup.sh
 export USE_CCACHE=1
 breakfast aosp_f5321-eng
-make -j$(nproc) halium-boot
-make -j$(nproc) recoveryimage
-make -j$(nproc) systemimage 
+make -i -j$(nproc) halium-boot
+make -i  -j$(nproc) recoveryimage
+make -i -j$(nproc) systemimage 
 
-echo "md5sum halium-boot.img and system.img"
-md5sum $ANDROID_ROOT/out/target/product/santoni/halium-boot.img
-md5sum $ANDROID_ROOT/out/target/product/santoni/recovery.img
-md5sum $ANDROID_ROOT/out/target/product/santoni/system.img
+
