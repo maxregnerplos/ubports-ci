bc8ebb0fa1f5b8c73676f96b49b993cd5d2dcf06
MaxRegner <59399856+MaxRegner@users.noreply.github.com>
2022-12-13 15:58:13 +0100

    Update build-hal.sh

--- build-hal.sh
+++ build-hal.sh
@@ -4,7 +4,7 @@ cd $ANDROID_ROOT
 
 source build/envsetup.sh
 export USE_CCACHE=1
-breakfast aosp_f5321-eng
+lunch aosp_f5321-eng
 make -i -j$(nproc) halium-boot
 make -i  -j$(nproc) recoveryimage
 make -i -j$(nproc) systemimage 
